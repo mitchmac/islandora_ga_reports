3d27ca94ca5e6f9131740345dac07bebd0beaa80
Phil Soga <phil@discoverygarden.ca>
2013-02-11 15:02:13 -0400

    Changed the url of the search
    changed islandora/solr/search to islandora/search

--- js/track_ds_downloads.js
+++ js/track_ds_downloads.js
@@ -8,7 +8,7 @@
       });
       
       //this is for the google analytics to track site searches
-      var path = window.location.pathname;      if (path.indexOf("islandora/solr/search") != -1) {      var r = new RegExp("/islandora/solr/search/([^/]*)/?(.*)?");      var m = path.match(r);      _gaq.push(['_trackPageview', '/islandora/solr/search/?q='+m[1]]);      _gaq.push(["_setAccount", ""]);}
+      var path = window.location.pathname;      if (path.indexOf("islandora/search") != -1) {      var r = new RegExp("/islandora/search/([^/]*)/?(.*)?");      var m = path.match(r);      _gaq.push(['_trackPageview', '/islandora/search/?q='+m[1]]);      _gaq.push(["_setAccount", ""]);}
     }
   };
 
