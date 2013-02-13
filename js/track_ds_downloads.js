decad4ab5c85ef1cdd4ff4ca3524b6ab1907fee9
Phil Soga <phil@discoverygarden.ca>
2013-02-13 12:43:05 -0400

    added tracking for facets using categories

--- js/track_ds_downloads.js
+++ js/track_ds_downloads.js
@@ -8,8 +8,46 @@
       });
       
       //this is for the google analytics to track site searches
-      var path = window.location.pathname;      if (path.indexOf("islandora/search") != -1) {      var r = new RegExp("/islandora/search/([^/]*)/?(.*)?");      var m = path.match(r);      _gaq.push(['_trackPageview', '/islandora/search/?q='+m[1]]);      _gaq.push(["_setAccount", ""]); }
+      var path = window.location.pathname;       if (path.indexOf("islandora/search") != -1) {
+        path = decodeURIComponent(path);
+        
+        var trackPageViewString = '/islandora/search/';
+        path = path.replace(/"/g,'#'); 
+        var facetRegEx = new RegExp(/\#(.*?)\#/g);
+        var facetArray=path.match(facetRegEx);
+        if (facetArray){
+          for (var i = 0; i<facetArray.length;i++)
+          {
+            facetArray[i] = facetArray[i].replace(/#/g,''); 
+
+          }
+        }
+
+        //regular expression for simple search
+        var basicRegex = new RegExp(/islandora\/search\/([^}]*)\?/);
+        //regular expression for advanced search
+        var advancedRegex = new RegExp(/\((.*?)\)/);
+        //try and match the simple search
+        var m= path.match(basicRegex);
+        //if there is not result try the advanced regular expression
+        if (!m ){
+          var m= path.match(advancedRegex);
+        }
+        trackPageViewString = trackPageViewString.concat('?q='+m[1]);
+
+        if (facetArray){
+          for (var j = 0; j < facetArray.length;j++)
+          {
+            trackPageViewString = trackPageViewString.concat( '&c='+facetArray[j]);
+          }
+        }
+      }
+        
+        
+              _gaq.push(['_trackPageview', '/islandora/search/'+trackPageViewString]);      _gaq.push(["_setAccount", ""]);       }
     }
   };
 
 })(jQuery);
+
+
